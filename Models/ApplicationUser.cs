33826540d00d3c91026992acd4c93f39e850f0f8
shiva2ziniosedge-gamedev <shiva2ziniosedge@gmail.com>
2026-04-27 10:45:36 +0530

    Remove authentication - not being used yet

--- Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace LiveChatApp.Models
-{
-    public class ApplicationUser:IdentityUser
-    {
-        public string? DisplayName {get;set;}
-    }
-}
